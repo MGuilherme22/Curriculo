7cfb2d956ada88fa9a13d7ae68e1607f4ce08d14
Marcos Guilherme <guitto10@outlook.com>
2024-05-17 19:55:20 -0300

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+var name = "Guilherme";
+console.log(name);
+console.log(name, 'é aluno da UNIT');
+var n1 = 7;
+var n2 = 5;
+var media = (n1 + n2) / 2;
+if (media >= 7){
+  console.log('O aluno', name, 'ficou com média:', media,', então ele está aprovado!')
+}
+else {
+  console.log('O aluno', name, 'ficou com média:', media,', então ele está reprovado!')
+ };
